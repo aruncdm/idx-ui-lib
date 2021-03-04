fa5fe54041b0e301930ff66b6a07ab704c79925d
Arunkumar Sivaramasethu <arunkumar1.s@intellectdesign.com>
2021-03-04 13:25:05 +0530

    adding idx button

---
+++ IdxButton.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <v-btn
+    v-bind="$attrs"
+    :value="value"
+      color="primary"
+    > {{title}}
+    </v-btn>
+
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: ['title']
+}
+</script>
